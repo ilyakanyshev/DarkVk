24dc99849270dbef68ad4904a87ef1e58039db92
Ilya Kanyshev <26299517+ilyakanyshev@users.noreply.github.com>
2017-08-31 09:34:06 +0300

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.onload = function () {
+setInterval(function(){
+if (document.getElementById('wide_column')) {
+var width = document.getElementById('wide_column').offsetWidth;
+}
+if (document.getElementById('page_body')) {
+var width = document.getElementById('page_body').offsetWidth;
+}
+if (document.getElementById('im_dialogs')) {
+var width = document.getElementById('im_dialogs').offsetWidth;
+}
+$("#audio_layer_tt").width(width);
 var width = document.body.offsetWidth; 
 document.getElementsByClassName('page_cover crisp_image _page_cover');
 if (width>1292) {
@@ -37,20 +48,5 @@ $("#audio_layer_tt").width(width);
 //$(".page_post_sized_thumbs.clear_fix").marginLeft('50%');
 $("div.page_post_sized_thumbs .clear_fix").width("100%");
 $('.audio_section.audio_w_covers._audio_section._audio_section__current audio_section__current').width('calc(100% - 44px)');
-};
-setInterval(function(){
-if (document.getElementById('wide_column')) {
-var width = document.getElementById('wide_column').offsetWidth;
-}
-if (document.getElementById('page_body')) {
-var width = document.getElementById('page_body').offsetWidth;
-}
-if (document.getElementById('im_dialogs')) {
-var width = document.getElementById('im_dialogs').offsetWidth;
-}
-$("#audio_layer_tt").width(width);
-
-
-
-
 },100);
+};
