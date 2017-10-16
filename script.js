1c70fe5a43b419a76fc5d057a3584ce954d120c3
Ilya Kanyshev <26299517+ilyakanyshev@users.noreply.github.com>
2017-10-16 19:41:15 +0300

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 setInterval(function(){
 if (document.getElementById('wide_column')) {
 var width = document.getElementById('wide_column').offsetWidth;
@@ -21,7 +19,7 @@ document.getElementById('dev_top_nav').style.width ='calc(100% - 284px)';
 }
 var width1 = document.body.offsetWidth;
 var width2 = document.getElementById('page_header').offsetWidth;
-$('.im-right-menu').css({right: (width1-width2+10)/2+"px"});
+$('.im-right-menu').css({right: (width1-width2+30)/2+"px"});
 
 if (document.getElementById('page_layout')) {
 document.getElementById('page_layout').style.width = 'calc(100% - 284px)'; 
@@ -49,9 +47,26 @@ $("div.page_cover.crisp_image._page_cover").height('200px');
 } 
 $("div.page_post_sized_thumbs .clear_fix").width("100%");
 $('.audio_section.audio_w_covers._audio_section._audio_section__current audio_section__current').width('calc(100% - 60px)');
-
 var width = document.getElementById('page_layout').offsetWidth;
-$("#audio_layer_tt").width(width-169+'px');
-$('._audio_page_content_block_wrap.audio_page_content_block_wrap.ui_scroll_container.ui_scroll_default_theme').css({width:'100%'})
+$("#audio_layer_tt").width(width-195+'px');
+$('.im-page_classic.im-page .im-page--top-date-bar-wrap').css({width: '100%' })
+$('._audio_page_content_block_wrap.audio_page_content_block_wrap.ui_scroll_container.ui_scroll_default_theme').css({width: '100%'})
 },100);
 
+window.AudioPlayerHTML5WebAudio.AudioPlayerHTML5.prototype._setFadeVolumeInterval = function(t) {
+        if (t) {
+            if (!this._fadeVolumeWorker && window.Worker && window.Blob) {
+                var e = new Blob(["           var interval;           onmessage = function(e) {             clearInterval(interval);             if (e.data == 'start') {               interval = setInterval(function() { postMessage({}); }, 20);             }           }         "]);
+                try {
+                    this._fadeVolumeWorker = new Worker(window.URL.createObjectURL(e))
+                } catch (i) {
+                    this._fadeVolumeWorker = !1
+                }
+            }
+            this._fadeVolumeWorker ? (this._fadeVolumeWorker.onmessage = t,
+            this._fadeVolumeWorker.postMessage("start")) : this._fadeVolumeInterval = setInterval(t, 360)
+        } else
+            this._fadeVolumeWorker && (this._fadeVolumeWorker.terminate(),
+            this._fadeVolumeWorker = null),
+            this._fadeVolumeInterval && clearInterval(this._fadeVolumeInterval)
+    }
